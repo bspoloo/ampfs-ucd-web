835fe9443f5a0a09fa622fa7b84cc5eb9b1ec802
DgteMendez <pilotomendez123@gmail.com>
2026-05-18 11:45:16 -0400

    Uploading front and back integration

--- app/hooks/use-get-data.ts
+++ app/hooks/use-get-data.ts
@@ -1,16 +1,28 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
 import { getData } from "../services/get.data.service";
 import { DataServer } from "../interfaces/data-response-list";
 
 export function useGetData<T>(endpoint: string, id?: string): DataServer<T> {
+    const {data: session, status} = useSession();
     const [data, setData] = useState<T | null>(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string>();
     
     useEffect(()=> {
-        getData<T>(endpoint)
+        if (status === "loading") {
+            return;
+        }
+
+        if (!session?.accessToken) {
+            setLoading(false);
+            setError("No autenticado");
+            return;
+        }
+
+        getData<T>(endpoint, session.accessToken)
             .then(data => setData(data))
             .catch((err)=> {
                 setError(err.message ?? "Error en traer la lista de datos")
--- app/services/get.data.service.ts
+++ app/services/get.data.service.ts
@@ -1,15 +1,21 @@
-export async function getData<D>(endpoint: string): Promise<D> {
+export async function getData<D>(
+    endpoint: string,
+    token: string,
+): Promise<D> {
     try {
         const API_BACKEND = process.env.BACKEND_API_URL;
         const res = await fetch(`${API_BACKEND}/${endpoint}`, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
             }
         });
+
         if (!res.ok) {
             throw new Error("Error al obtener los datos requeridos");
         }
+        
         return res.json();
     } catch (e) {
         const errorMessage = e instanceof Error ? e.message : String(e);
